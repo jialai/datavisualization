7eece5d88162b80109ae89f67b7ab28c8db91a47
jialai <jiajia_lai@126.com>
2018-09-19 21:42:10 +0200

    Initial commit

---
+++ src/main/java/com/myapp/datavisualization/DatavisualizationApplication.java
@@ -0,0 +1,12 @@
+package com.myapp.datavisualization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatavisualizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatavisualizationApplication.class, args);
+    }
+}
